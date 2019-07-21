bed4b68a9e19cff7f9093244eb7f861df4362de1
cosunae <carlos.osuna@meteoswiss.ch>
2019-07-21 11:44:19 +0200

    first commit

---
+++ scripts/kafka_init.sh
@@ -0,0 +1,4 @@
+/software/kafka_2.12-2.2.0/bin/zookeeper-server-start.sh /software/kafka_2.12-2.2.0/config/zookeeper.properties
+/software/kafka_2.12-2.2.0/bin/kafka-server-start.sh /software/kafka_2.12-2.2.0/config/server.properties 
+
+
