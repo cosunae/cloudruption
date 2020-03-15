21357414083a251c5d621ce69a262de442a8d242
cosunae <carlos.osuna@meteoswiss.ch>
2020-03-15 14:15:01 +0100

    container changes

--- ProducerConsumer/aws/entrypoint.sh
+++ ProducerConsumer/aws/entrypoint.sh
@@ -1,3 +1,3 @@
-mkdir /data/cosmo-7
+mkdir /cdata/cosmo-7
 aws s3 cp s3://cloudruption-pc/cosmo-7/laf2018020200 /data/cosmo-7
 /home/cloudruption/ProducerConsumer/build/producer /home/cloudruption/config/config.json
