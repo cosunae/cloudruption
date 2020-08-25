ac51b438b0a403b25b77e8b23dd95766bf9eb37f
Ubuntu <ubuntu@ip-172-31-42-40.eu-central-1.compute.internal>
2020-08-25 10:17:00 +0000

    parametrized the cleanup

--- scripts/cleanup_topics.sh
+++ scripts/cleanup_topics.sh
@@ -1,7 +1,9 @@
-topics=`/software/kafka_2.12-2.3.0/bin/kafka-topics.sh --list --bootstrap-server localhost:9092`
+kafkadir="/software/kafka_2.12-2.6.0"
+kafkabroker="b-2.cosmo-kafka.hifwlq.c4.kafka.eu-central-1.amazonaws.com:9092,b-1.cosmo-kafka.hifwlq.c4.kafka.eu-central-1.amazonaws.com:9092,b-3.cosmo-kafka.hifwlq.c4.kafka.eu-central-1.amazonaws.com:9092"
+topics=`${kafkadir}/bin/kafka-topics.sh --list --bootstrap-server $kafkabroker`
 
 for atopic in $topics; do
 
   echo "Deleting ... $atopic"
-  /software/kafka_2.12-2.3.0/bin/kafka-topics.sh --delete --topic $atopic  --bootstrap-server localhost:9092
+  ${kafkadir}/bin/kafka-topics.sh --delete --topic $atopic  --bootstrap-server $kafkabroker
 done
