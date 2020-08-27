c54a54e72d8c9d5b5945d2287b03d524cd286a6f
cosunae <carlos.osuna@meteoswiss.ch>
2020-08-27 17:51:27 +0200

    remove print strip

--- python/dataregistry.py
+++ python/dataregistry.py
@@ -280,9 +280,9 @@ class DataRegistryStreaming(DataRegistry):
             requestHandle = DataRegistry.subscribeIfNotExists(self, msKey.key)
             assert requestHandle
         for groupId, groupRequests in enumerate(self.groupRequests_):
-            self.verboseprint_("checking a message with key ", msKey.key, msKey.key.strip('\x00')," among requests of fields:", [x.name for x in groupRequests.reqFields_])
-            if msKey.key.strip('\x00') in [x.name for x in groupRequests.reqFields_]:
-                self.verboseprint_(" ... inserting data patch:",msKey.key.strip())
+            self.verboseprint_("checking a message with key ", msKey.key, " among requests of fields:", [x.name for x in groupRequests.reqFields_])
+            if msKey.key in [x.name for x in groupRequests.reqFields_]:
+                self.verboseprint_(" ... inserting data patch:",msKey.key)
                 field = msKey.key
                 self.insertDataPatch(RequestHandle(groupId, msKey.datetime), field,
                                      fieldop.SinglePatch(msKey.ilonstart, msKey.jlatstart, msKey.lonlen, msKey.latlen,
